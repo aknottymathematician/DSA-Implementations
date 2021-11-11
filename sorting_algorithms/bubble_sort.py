a8e74e8f4736a75f1d223974ecccd58e2610235b
aknottymathematician <aknottymathematician@gmail.com>
2021-11-12 01:06:54 +0530

    searching algos

--- sorting_algorithms/bubble_sort.py
+++ sorting_algorithms/bubble_sort.py
@@ -1,11 +1,25 @@
 def bubbleSort(arr):
 	n = len(arr)
+	"""
+	Traverse through the whole list
+	"""
 	for i in range(n):
+		"""
+		This provision is specially if the array is already sorted, 
+		thus giving us the best case scenario 
+		"""
 		swapped = False
+		"""
+		Traverse from 0 to n-i-1 since elements post i are already sorted 
+		and then swap if incorrectly placed
+		"""
 		for j in range(0, n-i-1):
 			if arr[j]>arr[j+1]:
 				arr[j], arr[j+1] = arr[j+1], arr[j]
 				swapped = True
+		"""
+		Break the loop in case the input list is already sorted
+		"""
 		if swapped == False:
 			break
 

3db503e7fdf6c208f81f9bbc176f0500c70b2fd1
aknottymathematician <aknottymathematician@gmail.com>
2021-11-13 02:19:31 +0530

    selection sort

---
+++ sorting_algorithms/selection_sort.py
@@ -0,0 +1,16 @@
+arr = [64, 25, 12, 22, 11]
+
+
+for i in range(len(arr)):
+	min_idx=i
+	for j in range(i+1, len(arr)):
+		if arr[min_idx]>arr[j]:
+			min_idx=j
+	arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+
+print(arr)
+
+# print ("Sorted array")
+# for i in range(len(arr)):
+#     print("%d" %arr[i])

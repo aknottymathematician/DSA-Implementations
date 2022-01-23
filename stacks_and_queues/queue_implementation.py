7277eefd84a69d26d166806c97224c6656775ee7
aknottymathematician <aknottymathematician@gmail.com>
2022-01-23 12:48:15 +0530

    queue implementation

---
+++ stacks_and_queues/queue_implementation.py
@@ -0,0 +1,44 @@
+from collections import deque
+
+class Queue:
+    def __init__(self):
+        self.buffer = deque()
+    
+    def enqueue(self, value):
+        self.buffer.appendleft(value)
+    
+    def dequeue(self):
+        return self.buffer.pop()
+
+    def is_empty(self):
+        return len(self.buffer) == 0
+
+    def size(self):
+        return len(self.buffer)
+
+    def peek(self):
+        return self.buffer[-1]
+
+pq = Queue()
+
+pq.enqueue({
+    'company': 'Wall Mart',
+    'timestamp': '15 apr, 11.01 AM',
+    'price': 131.10
+})
+pq.enqueue({
+    'company': 'Wall Mart',
+    'timestamp': '15 apr, 11.02 AM',
+    'price': 132
+})
+pq.enqueue({
+    'company': 'Wall Mart',
+    'timestamp': '15 apr, 11.03 AM',
+    'price': 135
+})
+
+print(pq.size())
+
+print(pq.dequeue())
+
+print(pq.peek())

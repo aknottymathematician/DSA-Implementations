9ed3c750751d20f5e1091e03f94cfcc23f5cb6a3
aknottymathematician <aknottymathematician@gmail.com>
2022-02-13 12:55:23 +0530

    stairway k steps

--- dynamic_programming/staircase.py
+++ dynamic_programming/staircase.py
@@ -4,7 +4,7 @@ You are climbing a staircase. It takes n steps to reach the top.
 Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 '''
 
-#Time Complexity - O(N); Space Complexity - O(N) 
+#Time Complexity - O(n); Space Complexity - O(n) 
 def stairway1(n):
 	dp = (n+1)*[None]
 	dp[0] = 1
@@ -16,7 +16,7 @@ def stairway1(n):
 	return dp[n]
 
 
-#Time Complexity - O(N); Space Complexity - O(1)
+#Time Complexity - O(n); Space Complexity - O(1)
 def stairway2(n):
 	a = 1
 	b = 1
@@ -26,4 +26,24 @@ def stairway2(n):
 		b = c 
 	return c
 
-print(stairway2(8))
+
+'''
+Follow Up Question - What if you can climb k steps at a time? s.t. k<n
+'''
+Time Complexity - O(nk); Space Complexity - O(n)
+def stairwayKSteps(n, k):
+	dp = (n+1)*[0]
+	dp[0] = 1
+	dp[1] = 1
+
+	for i in range(2, n+1):
+		#dp[i] = dp[i-1] + dp[i-2] + dp[i-3]+ ... +dp[i-k] 
+		for j in range(1,k+1):
+			if i-j<0:
+				continue
+			dp[i] += dp[i-j]
+	return dp[n]
+
+
+print(stairwayKSteps(8, 4))
+# print(stairway2(8))
